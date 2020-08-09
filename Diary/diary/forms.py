08ab75b92d0086575bb7f02881065968b68a5f20
jod35 <jodestrevin@gmail.com>
2020-08-09 13:45:26 -0700

    add (forms and routes + templates for the diary app)

---
+++ Diary/diary/forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField,PasswordField,StringField,BooleanField
+from wtforms.validators import DataRequired,EqualTo
+
+class RegistrationForm(FlaskForm):
+    username=StringField('Username',validators=[DataRequired()])
+    email=StringField('Email',validators=[DataRequired()])
+    password=PasswordField('Password',validators=[DataRequired(),EqualTo('confirm')])
+    confirm=PasswordField('Password',validators=[DataRequired()])
+
+
+    
+class LoginForm(FlaskForm):
+    username=StringField('Username',validators=[DataRequired()])
+    password=PasswordField('Password',validators=[DataRequired(),EqualTo('confirm')])
+    remember_me=BooleanField('Keep Me Logged In')
+
+class EventCreation(FlaskForm):
+    name=StringField('What You did',validators=[DataRequired()])
+    content=TextAreaField('Explain It',validators=[DataRequired()])
+   
--- Diary/diary/routes.py
+++ Diary/diary/routes.py
@@ -3,4 +3,27 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    context={
+        'title':"Welcome"
+    }
+    return render_template('index.html',**context)
+
+
+@app.route('/login')
+def sign_in():
+    context={
+        'title':'Login'
+    }
+    return render_template('login.html',**context)
+
+
+@app.route('/signup')
+def create_account():
+    context={
+        'title':'Create Your Free Account'
+    }
+    return render_template('signup.html',**context)
+
+@app.route('/add_event')
+def add_event():
+    pass
