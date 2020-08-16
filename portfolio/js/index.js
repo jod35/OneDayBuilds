3d34045dacd61249d7f23404f6dec131c4af55ca
jod35 <jodestrevin@gmail.com>
2020-08-16 13:39:17 +0300

    work on the header

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -1,7 +1,23 @@
-const scroll_navigation=document.querySelector('nav');
-const navigation=document.querySelector('.top-nav');
+const navigation_bar=document.querySelector('.scroll-nav-bar');
+const nav_links=document.querySelectorAll('.nav-item');
 
-if (document.body.scrollTop >80 || document.documentElement.scrollTop>80){
-    scroll_navigation.style.display = "flex";
-    navigation.style.display="none";
+window.onscroll=function(){documentScroll()};
+function documentScroll(){
+    if(document.body.scrollTop>80 || document.documentElement.scrollTop>80){
+        navigation_bar.style.backgroundColor="white";
+
+
+
+        for (let i of nav_links){
+            i.style.color="rgb(172, 65, 26)";
+            i.style.padding="8px";
+        }
+    }
+    else{
+        navigation_bar.style.backgroundColor="rgb(172, 65, 26)";
+        for (let i of nav_links){
+           i.style.color="white";
+           i.style.backgroundColor=""
+        }
+    }
 }
